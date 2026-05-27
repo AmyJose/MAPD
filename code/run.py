a33290e8ea1815fbd4f80894c8c95e513887c52c
AmyJose <helloamyjose@gmail.com>
2026-05-27 10:50:53 +0100

    adding results folder

--- code/run.py
+++ code/run.py
@@ -1,4 +1,8 @@
 from model import SpaceModel
+from pathlib import Path
+from datetime import datetime
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 model = SpaceModel(10, 10)
 
@@ -17,8 +21,11 @@ worker_data = agent_data[
     agent_data["Agent Type"] == "WorkerAgent"
 ]
 
-print(model_data.tail())
-print(worker_data.tail())
+output_dir = Path("results") / timestamp
+output_dir.mkdir(parents=True, exist_ok=True)
+
+model_data.to_csv(output_dir / "model_data.csv")
+worker_data.to_csv(output_dir / "worker_data.csv")
 
 print(f"Generated tasks: {model.generated_tasks}")
 print(f"Completed tasks: {model.completed_tasks}")
