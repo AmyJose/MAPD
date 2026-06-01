495784dd96ed36e0655ab662f00e6af58977e6de
AmyJose <helloamyjose@gmail.com>
2026-06-01 16:17:51 +0100

    updating visualisation code

--- code/app.py
+++ code/app.py
@@ -1,9 +1,17 @@
-import solara
-
 from mesa.visualization import SolaraViz, make_space_component
+
 from model import SpaceModel
 from agent import WorkerAgent
-from markers import PickupMarker, DropoffMarker, BlockedCellMarker, PathMarker, ParkingMarker
+from markers import (
+    PickupMarker,
+    DropoffMarker,
+    BlockedCellMarker,
+    PathMarker,
+    ParkingMarker,
+)
+
+
+SCENARIO = "standard"
 
 
 def agent_portrayal(agent):
@@ -15,52 +23,36 @@ def agent_portrayal(agent):
         else:
             color = "grey"
 
-        return {
-            "color": color,
-            "size": 80,
-            "marker":"o"
-        }
+        return {"color": color, "size": 80, "marker": "o"}
 
     if isinstance(agent, PickupMarker):
-        return {
-            "color": "orange",
-            "size": 60,
-            "marker":"s"
-        }
+        return {"color": "orange", "size": 60, "marker": "s"}
 
     if isinstance(agent, DropoffMarker):
-        return {
-            "color": "red",
-            "size": 60,
-            "marker":"X"
-        }
-    
-    if isinstance(agent, BlockedCellMarker):
-        return {
-            "color": "black",
-            "size": 100,
-            "marker": "s",
-        }
-    if isinstance(agent, ParkingMarker):
-        return {
-            "color": "grey",
-            "size": 100,
-            "marker": "s",
-        }
-    
-    if isinstance(agent, PathMarker):
-        return {
-        "color": "lightblue",
-        "size": 25,
-        "marker": ".",
-    }
+        return {"color": "red", "size": 60, "marker": "X"}
 
-model = SpaceModel(width=10, height=10)
+    if isinstance(agent, BlockedCellMarker):
+        return {"color": "black", "size": 100, "marker": "s"}
+
+    if isinstance(agent, ParkingMarker):
+        return {"color": "grey", "size": 100, "marker": "s"}
+
+    if isinstance(agent, PathMarker):
+        return {"color": "lightblue", "size": 25, "marker": "."}
+
+    return {"color": "white", "size": 20, "marker": "."}
+
+
+model = SpaceModel(
+    width=10,
+    height=10,
+    scenario=SCENARIO,
+)
 
 space_component = make_space_component(agent_portrayal)
 
 page = SolaraViz(
     model,
     components=[space_component],
-    name="MAPD Toy Model",
+    name=f"MAPD Toy Model ({SCENARIO})",
 )
